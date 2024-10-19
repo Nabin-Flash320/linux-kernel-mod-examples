5b98662395a977cd48a935e421abab96a0fd2564
Nabin-Flash320 <nabindangi32@gmail.com>
2024-10-19 16:59:37 +0545

    Simple kernel module that accepts arguments

--- Makefile
+++ Makefile
@@ -1,9 +1,19 @@
+
+
 # Define the object file to be built
 obj-m := main.o
 
 # Default rule to build the module
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+
+# Rule to insert the module
+load:
+	sudo insmod main.ko
+
+# Rule to remove the module
+unload:
+	sudo rmmod main
 
 # Clean up the generated files
 clean:
--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <linux/module.h> // Required for all kernel modules
 #include <linux/kernel.h> // Required for KERN_INFO
 #include <linux/init.h>   // Required for the macros
-#include <linux/list.h>
 #include <linux/printk.h>
 #include <linux/moduleparam.h>
+#include <linux/stat.h>
+
+// Module information
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Nabin-Flash320");
+MODULE_DESCRIPTION("A Simple Hello World Kernel Module");
+
+static short int int_val = 10;
+
+module_param(int_val, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(int_val, "Integer value that holds the short int type value from command line");
 
 // Function that executes when the module is loaded
 static int __init hello_init(void)
@@ -12,7 +22,7 @@ static int __init hello_init(void)
         Shortcut to:
         printk(KERN_INFO fmt...);
     */
-    pr_info("Hello, World! Kernel Module Loaded.\n");
+    pr_info("Command line arg for int_val %d\n", int_val);
     return 0; // Return 0 if successful
 }
 
@@ -26,7 +36,3 @@ static void __exit hello_exit(void)
 module_init(hello_init);
 module_exit(hello_exit);
 
-// Module information
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Nabin-Flash320");
-MODULE_DESCRIPTION("A Simple Hello World Kernel Module");
